6e6ab072d4d87c9bf782a77055c8128c75cc3ded
Felipe Wai <felipe.wai@t2s.com.br>
2025-03-23 13:24:29 -0300

    feat: produtos com foto

--- src/main/java/com/horto/backend/infra/mapper/ProductMapper.java
+++ src/main/java/com/horto/backend/infra/mapper/ProductMapper.java
@@ -1,16 +1,20 @@
 package com.horto.backend.infra.mapper;
 
 import com.horto.backend.core.entities.Product;
+import com.horto.backend.core.entities.ProductPicture;
 import com.horto.backend.infra.dto.product.request.ProductRequestDTO;
 import com.horto.backend.infra.dto.product.response.ProductResponseDTO;
 import com.horto.backend.infra.persistence.entities.ProductEntity;
+import com.horto.backend.infra.persistence.entities.ProductPictureEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = ProductPictureMapper.class)
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-    @Mapping(target = "pictures", qualifiedByName = "mapWithoutProduct")
+    @Mapping(target = "product", ignore = true)
+    ProductPicture toDomain(ProductPictureEntity entity);
+
     Product toDomain(ProductEntity entity);
 
     ProductResponseDTO toResponseDTO(Product product);
