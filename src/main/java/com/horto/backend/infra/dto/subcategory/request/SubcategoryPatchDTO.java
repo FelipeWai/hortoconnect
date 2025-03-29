547c4b5feb0fd91a42a1ecd197fbf01649780031
Felipe Wai <felipe.wai@t2s.com.br>
2025-03-28 22:07:38 -0300

    feat: subcategory patch dto validation

--- src/main/java/com/horto/backend/infra/dto/subcategory/request/SubcategoryPatchDTO.java
+++ src/main/java/com/horto/backend/infra/dto/subcategory/request/SubcategoryPatchDTO.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public record SubcategoryPatchDTO(
-        Optional<String> name,
+        Optional<@Size(min = 1, max = 50, message = "Tamanho do nome de usuário deve estar entre 1 e 50 caracteres.") String> name,
         Optional<Long> category_id,
         Optional<@Size(min = 1, message = "A subcategoria deve ter pelo menos uma qualidade") List<Long>> qualities_id,
         Optional<@Size(min = 1, message = "A subcategoria deve ter pelo menos um tamanho") List<Long>> sizes_id
